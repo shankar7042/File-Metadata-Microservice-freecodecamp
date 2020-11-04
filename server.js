5b450d16519158d94a842afc77a7cba8ac8b37b1
shankar7042 <shankar.kumar.4171@gmail.com>
2020-11-04 15:36:56 +0530

    file upload info using multer

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const uploads = multer({ dest: "./uploads" });
+// require and use "multer"...
+
+const app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post("/api/fileanalyse", uploads.single("upfile"), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Node.js listening ...");
+});
